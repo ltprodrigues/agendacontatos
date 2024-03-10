8fc2314f14eae12f0bfddcf5be3766458e6a0ae1
Leticia Rodrigues <leticiateixeirar@gmail.com>
2024-03-10 09:36:09 -0400

    commit inicial

---
+++ main.js
@@ -0,0 +1,26 @@
+function adicionarContato() {
+    // Obter os valores do formulário
+    var nome = document.getElementById("nome").value;
+    var telefone = document.getElementById("telefone").value;
+
+    // Validar se ambos os campos foram preenchidos
+    if (nome && telefone) {
+      // Criar uma nova linha na tabela
+      var tabela = document.getElementById("tabelaContatos");
+      var novaLinha = tabela.insertRow(-1);
+
+      // Inserir células na nova linha
+      var celulaNome = novaLinha.insertCell(0);
+      var celulaTelefone = novaLinha.insertCell(1);
+
+      // Preencher as células com os valores do formulário
+      celulaNome.innerHTML = nome;
+      celulaTelefone.innerHTML = telefone;
+
+      // Limpar os campos do formulário
+      document.getElementById("nome").value = "";
+      document.getElementById("telefone").value = "";
+    } else {
+      alert("Por favor, preencha ambos os campos.");
+    }
+  }
